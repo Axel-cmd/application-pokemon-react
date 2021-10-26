ce4fb9f3df463abc89691634a985e70136da5f6f
Axel-MRT <77530883+Axel-cmd@users.noreply.github.com>
2021-10-26 14:17:33 +0200

    Récupération de pokemons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
 import './App.css';
-import { Select } from '@material-ui/core'
+// import { Select } from '@material-ui/core'
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  const [allPokemon, setAllPokemons] = useState([])
+  const [loadPokemon, setLoadPokemon] = useState('https://pokeapi.co/api/v2/pokemon?limit=20');
+
+  const getAllPokemons = async () => {
+    const res = await fetch(loadPokemon)
+    const data = await res.json()
+
+    setLoadPokemon(data.next)
+    // console.log(data)
+
+    function createPokemonObject(result)
+    {
+      result.forEach( async (pokemon) => {
+        const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
+        const data = await res.json()
+
+        setAllPokemons(currentList => [...currentList, data])
+      
+      });
+    }
+    
+    createPokemonObject(data.results)
+    await console.log(allPokemon);
+  }
+
+  
+  useEffect(() => {
+    getAllPokemons()
+  }, [])
+
   return (
     <div className="App">
-      test
-      <Select></Select>
+      {console.log(allPokemon)}
     </div>
   );
 }
