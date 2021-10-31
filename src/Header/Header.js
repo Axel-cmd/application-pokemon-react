47d47635c19a9d046269fe5704dabfd47a670267
William_Mt <Williammoutard@gmail.com>
2021-10-31 19:39:53 +0100

    Header

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { AppBar,  } from '@material-ui/core'
+import { AppBar, Toolbar, Box, IconButton, Typography, } from '@material-ui/core'
 import './Header.css'
 
 
@@ -7,10 +7,10 @@ export default function Header(){
 
   return(
 <div className="container-header">
+  <img className="img" src={`${process.env.PUBLIC_URL}/logo/pikacu.jpg`}alt=""/>
+    <img className="img" src={`${process.env.PUBLIC_URL}/logo/pokemon.png`}alt=""/>
 
-  <p> testt</p>
-</div>
 
-
-  )
+  </div>
+)
 }
