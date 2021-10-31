5b9d378e1474b3cc5bed6c0c77ed5f310251bef9
Axel-MRT <77530883+Axel-cmd@users.noreply.github.com>
2021-10-31 17:17:49 +0100

    Récupération du pokémon
    Appel sur l'api pour récupérer le pokémon + bouton de retour au menu /home

--- src/pokemonDetail/pokemonDetail.js
+++ src/pokemonDetail/pokemonDetail.js
@@ -1,15 +1,34 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import {
-    useParams
+    useParams,
+    Link
   } from "react-router-dom"
+import { Button } from '@material-ui/core';
+
+
 export default function PokemonDetail ()
 {
     //récupérer le paramètre id passé dans l'url
     const { id } = useParams();
+    
+    
+    const getPokemon = async () =>
+    {
+        const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+        const result = await res.json();
+
+        console.log(result);
+    }
+
+    useEffect(() => {
+        getPokemon();
+    }, [])
+
 
     return(
-
-        <p> autre page {id} </p>
-
+        <>
+            <Link className="linkStyle" to="/home"><Button>Back</Button></Link>
+            <p> autre page {id} </p>
+        </>
     )
 }
