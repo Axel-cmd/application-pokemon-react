d167b6852d915749cb3271f36e8d1ca2cdcb77b0
Axel-MRT <77530883+Axel-cmd@users.noreply.github.com>
2021-11-01 17:07:37 +0100

    Update pokemonDetail.js

--- src/pokemonDetail/pokemonDetail.js
+++ src/pokemonDetail/pokemonDetail.js
@@ -3,7 +3,7 @@ import {
     useParams,
     Link
   } from "react-router-dom"
-import { Button, Grid, Paper } from '@material-ui/core';
+import { Button, Grid, Paper, Avatar } from '@material-ui/core';
 import PokemonCard from '../componentPokemon/pokemonCard'
 
 
@@ -11,17 +11,19 @@ export default function PokemonDetail ()
 {
     //récupérer le paramètre id passé dans l'url
     const { id } = useParams();
-    
+    const [growth, setGrowth] =  useState();
+    const [species, setSpecies] =  useState();
     const [pokemon, setPokemon] =  useState();
-    // const [nextPokemon, setNextPokemon] = useState();
-    // const [previousPokemon, setPreviousPokemon] = useState();
+    const [show, setShow] =  useState(false);
+    const [nextPokemon, setNextPokemon] = useState();
+    const [previousPokemon, setPreviousPokemon] = useState();
 
-
-    const getPokemon = async (id) =>
+    
+    const getPokemon = async (url) =>
     {
 
         return new Promise((resolve, reject) => {
-            fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+            fetch(url)
             .then(res => res.json())
             .then(data => {
                 resolve(data);
@@ -40,20 +42,27 @@ export default function PokemonDetail ()
     
     const fetchData = async () => {
     
-        let response = await getPokemon(id);
-        setPokemon(response)
+        let response = await getPokemon(`https://pokeapi.co/api/v2/pokemon/${id}`);
+        setPokemon(response);
+        setShow(true);
         //traitement pour le next et previous
-        // if( id > 1 )
-        // { 
-        //     let previousResponse = await getPokemon(id-1);
-        //     setPreviousPokemon(previousResponse)
-        // }
-        // if( id <151){
-        //     // var nb = ;
-        //     let nextResponse = await getPokemon(parseInt(id)+1);
-        //     setNextPokemon(nextResponse);
-        // }
-        
+        if( id > 1 )
+        { 
+            let previousResponse = await getPokemon(`https://pokeapi.co/api/v2/pokemon/${id-1}`);
+            setPreviousPokemon(previousResponse)
+        }
+        if( id <151){
+            // var nb = ;
+            let nextResponse = await getPokemon(`https://pokeapi.co/api/v2/pokemon/${parseInt(id)+1}`);
+            setNextPokemon(nextResponse);
+        }
+
+        //growth
+        let growthResponse = await getPokemon(`https://pokeapi.co/api/v2/growth-rate/${id}/`);
+        setGrowth(growthResponse.name)
+
+        let pokemonSpecies = await getPokemon(`https://pokeapi.co/api/v2/pokemon-species/${id}/`);
+        setSpecies(pokemonSpecies)
     };
     
     
@@ -65,27 +74,30 @@ export default function PokemonDetail ()
 
 
     return(
-        
         <Paper style={{margin:10, marginTop:20}}>
+        {show && 
+        
         <Grid container spacing={2}>
 
             <Grid item container spacing={2} xs={12}>
                 
-                {/* {previousPokemon &&
-                    
-                <Grid item md={6} xs={6} style={{paddingLeft:20}}>
-                    <Link to={`/pokemon/${previousPokemon.id}`} onClick={fetchData()}>
-                    <Avatar alt={previousPokemon.name} src={previousPokemon.sprites.other.dream_world.front_default} />
-                    <p style={{fontSize:10}}>previous</p></Link>
-                </Grid> }
-                {nextPokemon && 
                 
-                <Grid item md={6} xs={6} style={{textAlign:'right'}}>
-                    <Link to={`/pokemon/${nextPokemon.id}`} onClick={fetchData()}>
-                    <Avatar alt={nextPokemon.name} src={nextPokemon.sprites.other.dream_world.front_default}/>
+                    
+                <Grid item md={11} xs={11} style={{paddingLeft:20}}>
+                {previousPokemon &&
+                    <Link to={`/pokemon/${previousPokemon.id}`} >
+                    <Avatar alt={previousPokemon.name} src={previousPokemon.sprites.other.dream_world.front_default} />
+                    <p style={{fontSize:10}}>previous</p></Link>}
+                </Grid> 
+                
+                
+                <Grid item md={1} xs={1} style={{textAlign:'right'}}>
+                {nextPokemon && 
+                    <Link to={`/pokemon/${nextPokemon.id}`} >
+                    <Avatar alt={nextPokemon.name} src={nextPokemon.sprites.other.dream_world.front_default} style={{paddingLeft:"70%"}}/>
                     <p style={{fontSize:10}}>next</p>
-                    </Link>
-                </Grid>} */}
+                    </Link>}
+                </Grid>
 
             </Grid>
            
@@ -109,16 +121,63 @@ export default function PokemonDetail ()
                 <Grid item container xs={10}>
 
                         <Grid item xs={6}>
-                            BIO
+                            <h2>Bio</h2>
+                            <p className="description">{ 
+                                `${pokemon.name}, ${pokemon.types[0].type.name} ${pokemon.types[1] && `and ${pokemon.types[1].type.name}`} pokemon.
+                                `
+                                }
+                                {<>
+                                    <p>Height :  {pokemon.height/10}m</p> 
+                                    <p>Weight :  {pokemon.weight/10}kg</p> 
+                                    <p>Abilities :  {pokemon.abilities.map((abilities, index) =>
+                                        
+                                            `${abilities.ability.name } `
+                                            // console.log(abilities)
+                                        
+                                    )} </p> 
+                                </>
+                                }
+
+
+                            </p>
                         </Grid>
-                        <Grid item xs={6}>
-                            Training
+                        <Grid item xs={6} >
+                            <h2>Training</h2>
+                            
+                                
+                            <p>Base exp : {pokemon.base_experience}</p>
+                            <p>Base happiness : {species.base_happiness}</p>  
+                            <p>Catch rate : {species.capture_rate}</p>
+                            <p>Growth rate : {growth}</p>
                         </Grid>
+
+
+
                         <Grid item xs={12}>
-                        Evolution
+                            <h2>Evolution</h2>
+
+                            {
+
+
+                            }
+
                         </Grid>
-                        <Grid item xs={12}>
-                            Stats
+
+
+
+                        <Grid item xs={12} container direction="column">
+                            <h2>Stats</h2>
+
+                            <Grid item container spacing={1}>
+                            {pokemon.stats.map((stat, index) =>
+                                <Grid item xs={2}>
+                                <p>{stat.stat.name}</p>
+                                <p>{stat.base_stat}</p>
+
+                                </Grid>
+                            )}
+                            </Grid>
+                            
                         </Grid>
 
                 </Grid>
@@ -134,8 +193,8 @@ export default function PokemonDetail ()
                 <Link className="linkStyle" to="/home"><Button>Back</Button></Link>
             </Grid>
         </Grid>
-        </Paper>
         
-
+        }
+        </Paper>
     )
 }
